764a5fe9738fdd1c2ebba5998e067b4ab9a99173
Ifeoma-Nwh <ifeoma.nwaoha10@gmail.com>
2024-06-10 13:02:21 +0200

    added table join between cowork and tag

---
+++ database/migrations/1718014223031_create_coworks_tags_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'coworks_tags'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+      table.integer('cowork_id').unsigned().references('coworks.id').notNullable()
+      table.integer('tag_id').unsigned().references('tags.id').notNullable()
+
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').nullable()
+
+      table.unique(['cowork_poi_id', 'tag_id'])
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
